e59f177ca0514df9dc0ccff0a70a50fa91aa2ac3
Angie Roach <angieroach4@gmail.com>
2019-09-05 19:50:56 -0400

    changed wins/losses from append to html to keep correct score

--- assets/js/game.js
+++ assets/js/game.js
@@ -59,7 +59,7 @@ function checkScore() {
 
         console.log("player won");
         $("#congrats").html("Congratulation! You won!");
-        $("#wins").append(wins);
+        $("#wins").html(wins);
         startGame();
     }
        
@@ -67,7 +67,7 @@ function checkScore() {
         losses++
         console.log("player lost");
         $("#congrats").html("Oh sorry, you lost!");
-        $("#losses").append(losses);
+        $("#losses").html("Your losses " + losses);
         startGame();
     }
 
