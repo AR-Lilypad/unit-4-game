cd923a33c266c26a7997b76cbef09c03a072b7f7
Angie Roach <angieroach4@gmail.com>
2019-09-05 18:55:14 -0400

    added start game functions, and check score functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,7 @@ var crystal1;
 var crystal2;
 var crystal3;
 var crystal4;
-var allCrystals;
+// var allCrystals;
 var wins = 0;
 var losses = 0;
 var playerScore = 0;
@@ -17,73 +17,71 @@ var playAgain = false;
 // make the random numbers to assign to variables  
 function startGame() {
     computerRandomNum = Math.floor(Math.random() * 150) + 21;     //random between 20 and 150 (20 because player could potentially lose on first click)
+    $("#computerRandomNum").html(computerRandomNum);                // put random number in circle on the html page
     crystal1 = Math.floor(Math.random() * 20) + 1;
     crystal2 = Math.floor(Math.random() * 20) + 1;
     crystal3 = Math.floor(Math.random() * 20) + 1;
     crystal4 = Math.floor(Math.random() * 20) + 1;               //crystal numbers should not be more than 20/21
     playerScore = 0;
+    $("#playerScore").html("Your crystals score is:   " + playerScore);
     console.log(computerRandomNum);
     console.log(playerScore);
+};
 
-    
-        $("#crystal1").on("click", function () {                    // user clicks on crystals 1 through 4 to generate random player numbers
-            playerScore += crystal1;
-            $("#playerScore").html("Your crystals score is:   " + playerScore);
-            console.log("Player Score: " + playerScore);
-        })
-        $("#crystal2").on("click", function () {
-            playerScore += crystal2;
-            $("#playerScore").html("Your crystals score is:   " + playerScore);
-            console.log("Player Score: " + playerScore);
-        })
-        $("#crystal3").on("click", function () {
-            playerScore += crystal3;
-            $("#playerScore").html("Your crystals score is:   " + playerScore);
-            console.log("Player Score: " + playerScore);
-        })
-        $("#crystal4").on("click", function () {
-            playerScore += crystal4;
-            $("#playerScore").html("Your crystals score is:   " + playerScore);
-            console.log("Player Score: " + playerScore);
-        })
-    
+
+$("#crystal1").on("click", function () {                    // user clicks on crystals 1 through 4 to generate random player numbers
+    playerScore += crystal1;
+    $("#playerScore").html("Your crystals score is:   " + playerScore);
+    console.log("Player Score: " + playerScore);
+    checkScore();
+})
+$("#crystal2").on("click", function () {
+    playerScore += crystal2;
+    $("#playerScore").html("Your crystals score is:   " + playerScore);
+    console.log("Player Score: " + playerScore);
+    checkScore();
+})
+$("#crystal3").on("click", function () {
+    playerScore += crystal3;
+    $("#playerScore").html("Your crystals score is:   " + playerScore);
+    console.log("Player Score: " + playerScore);
+    checkScore();
+})
+$("#crystal4").on("click", function () {
+    playerScore += crystal4;
+    $("#playerScore").html("Your crystals score is:   " + playerScore);
+    console.log("Player Score: " + playerScore);
+    checkScore();
+})
+function checkScore() {
     if (playerScore === computerRandomNum) {                             //player wins
         wins++;
+
         console.log("player won");
-        $("#congrats").html("Congratulation! You won!" + wins);
-        playAgain = confirm("Play Again?");
-
-        if (playAgain === true) {
-            startGame();
-            playerScore = 0;
-        }
-        else {
-            alert(" Ok, goodby. Please close your browser.");
-        }
+        $("#congrats").html("Congratulation! You won!");
+        $("#wins").append(wins);
+        startGame();
     }
-
-    if (playerScore > computerRandomNum) {                               //player loses
+       
+    else if (playerScore > computerRandomNum) {                               //player loses
         losses++
         console.log("player lost");
-        $("#computerRandomNum").html(computerRandomNum);
-        playAgain = confirm("Play Again?");
-
-        if (playAgain === true) {
-            startGame();
-            playerScore = 0;
-        }
-        else {
-            alert(" Ok, goodbye. Please close your browseer");
-        }
+        $("#congrats").html("Oh sorry, you lost!");
+        $("#losses").append(losses);
+        startGame();
     }
-};
+
+    else (
+        console.log("keep guessing!")
+    )
+}
 
 startGame();
 
-// put random number in circle on the html page
-$("#computerRandomNum").html(computerRandomNum);
 
-// put player score on the html page
+
+
+
 
 
 
