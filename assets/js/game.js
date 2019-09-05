cbdd8564f00ad04f7ed04a0a82e9505d20f8077d
Angie Roach <angieroach4@gmail.com>
2019-09-04 23:40:11 -0400

    fixed crystals score html display

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,7 @@ var crystal1;
 var crystal2;
 var crystal3;
 var crystal4;
+var allCrystals;
 var wins = 0;
 var losses = 0;
 var playerScore = 0;
@@ -24,27 +25,28 @@ function startGame() {
     console.log(computerRandomNum);
     console.log(playerScore);
 
-    $("#crystal1").on("click", function () {                    // user clicks on crystals 1 through 4 to generate random player numbers
-        playerScore += crystal1;
-        $("#playerScore").html("Your crystal score is:   " + playerScore);
-        console.log("Player Score: " + playerScore);
-    })
-    $("#crystal2").on("click", function () {
-        playerScore += crystal2;
-        $("#playerScore").html("Your crystal score is:   " + playerScore);
-        console.log("Player Score: " + playerScore);
-    })
-    $("#crystal3").on("click", function () {
-        playerScore += crystal3;
-        $("#playerScore").html("Your crystal score is:   " + playerScore);
-        console.log("Player Score: " + playerScore);
-    })
-    $("#crystal4").on("click", function () {
-        playerScore += crystal4;
-        $("#playerScore").html("Your crystal score is:   " + playerScore);
-        console.log("Player Score: " + playerScore);
-    })
-function chkNums()
+    
+        $("#crystal1").on("click", function () {                    // user clicks on crystals 1 through 4 to generate random player numbers
+            playerScore += crystal1;
+            $("#playerScore").html("Your crystals score is:   " + playerScore);
+            console.log("Player Score: " + playerScore);
+        })
+        $("#crystal2").on("click", function () {
+            playerScore += crystal2;
+            $("#playerScore").html("Your crystals score is:   " + playerScore);
+            console.log("Player Score: " + playerScore);
+        })
+        $("#crystal3").on("click", function () {
+            playerScore += crystal3;
+            $("#playerScore").html("Your crystals score is:   " + playerScore);
+            console.log("Player Score: " + playerScore);
+        })
+        $("#crystal4").on("click", function () {
+            playerScore += crystal4;
+            $("#playerScore").html("Your crystals score is:   " + playerScore);
+            console.log("Player Score: " + playerScore);
+        })
+    
     if (playerScore === computerRandomNum) {                             //player wins
         wins++;
         console.log("player won");
@@ -71,7 +73,7 @@ function chkNums()
             playerScore = 0;
         }
         else {
-            alert(" Ok, goodby. Please close your browseer");
+            alert(" Ok, goodbye. Please close your browseer");
         }
     }
 };
